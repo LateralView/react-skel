6e64376208bd952a0edb7111b57dae8eefadb840
Gabriel Casullo <info@casu-net.com.ar>
2018-11-13 08:55:23 -0300

    fix styling issues, also hide drawer when element is clicked

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,6 +6,8 @@ import AppBar from 'react-toolbox/lib/app_bar'
 import Drawer from 'react-toolbox/lib/drawer'
 import { MenuItem, MenuDivider } from 'react-toolbox/lib/menu'
 
+import style from './style.scss'
+
 export default class Header extends React.Component {
   static get propTypes() {
     return {
@@ -19,24 +21,39 @@ export default class Header extends React.Component {
   constructor() {
     super()
     this.state = { drawer: false }
+    this.toggleDrawer = this.toggleDrawer.bind(this)
+    this.editUser = this.editUser.bind(this)
+    this.logout = this.logout.bind(this)
+  }
+
+  toggleDrawer() {
+    this.setState({ drawer: !this.state.drawer })
+  }
+
+  editUser() {
+    this.toggleDrawer()
+    // TODO: implement
+    // this.props.onEdit()
+  }
+
+  logout() {
+    this.toggleDrawer()
+    this.props.onLogout()
   }
 
   render() {
     return (
-      <div>
-        <Drawer
-          active={this.state.drawer}
-          onOverlayClick={() => this.setState({ drawer: !this.state.drawer })}
-        >
+      <div className={style.headerWrapper}>
+        <Drawer active={this.state.drawer} onOverlayClick={this.toggleDrawer}>
           <UserInfo user={this.props.user} />
           <MenuDivider />
-          <MenuItem onClick={this.props.onEdit}>Edit User</MenuItem>
-          <MenuItem onClick={this.props.onLogout}>Logout</MenuItem>
+          <MenuItem onClick={this.editUser}>Edit User</MenuItem>
+          <MenuItem onClick={this.logout}>Logout</MenuItem>
         </Drawer>
         <AppBar
           title="React Skeleton"
           leftIcon={this.props.loggedIn ? 'menu' : null}
-          onLeftIconClick={() => this.setState({ drawer: !this.state.drawer })}
+          onLeftIconClick={this.toggleDrawer}
         />
       </div>
     )
